19a77d37276c69c97afc1082d911ba8edcdafc58
LONGZHEN <LONGZHEN@Clyde-PC>
2015-07-15 10:06:40 +1200

    clyde PG begin

---
+++ www/js/app.js
@@ -0,0 +1,95 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic'])
+
+        .run(function ($ionicPlatform) {
+            $ionicPlatform.ready(function () {
+                // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+                // for form inputs)
+                if (window.cordova && window.cordova.plugins.Keyboard) {
+                    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                }
+                if (window.StatusBar) {
+                    StatusBar.styleDefault();
+                }
+            })
+
+        })
+        .config(function ($stateProvider, $urlRouterProvider) {
+            $stateProvider
+                    .state('menus', {
+                        url: '/menus',
+                        abstract: true,
+                        templateUrl: 'templates/sidemenu.html'
+                    })
+
+                    .state('menus.home', {
+                        url: "/home",
+                        views: {
+                            'cvContent': {
+                                templateUrl: 'templates/home.html'
+                            }
+                        }
+                    })
+                    .state('menus.summary', {
+                        url: "/summary",
+                        views: {
+                            'cvContent': {
+                                templateUrl: 'templates/summary.html'
+                            }
+                        }
+                    })
+                   .state('menus.experience', {
+                       url: "/experience",
+                       views: {
+                           'cvContent': {
+                               templateUrl: 'templates/experience.html'
+                           }
+                       }
+                   })
+                   .state('menus.portfolio', {
+                       url: "/portfolio",
+                       views: {
+                           'cvContent': {
+                               templateUrl: 'templates/portfolio.html'
+                           }
+                       }
+                   })
+                   .state('menus.education', {
+                       url: "/education",
+                       views: {
+                           'cvContent': {
+                               templateUrl: 'templates/education.html'
+                           }
+                       }
+                   })
+                   .state('menus.reference', {
+                       url: "/reference",
+                       views: {
+                           'cvContent': {
+                               templateUrl: 'templates/reference.html'
+                           }
+                       }
+                   })
+                   .state('menus.social', {
+                       url: "/social",
+                       views: {
+                           'cvContent': {
+                               templateUrl: 'templates/social.html'
+                           }
+                       }
+                   })
+                   .state('menus.about', {
+                       url: "/about",
+                       views: {
+                           'cvContent': {
+                               templateUrl: 'templates/about.html'
+                           }
+                       }
+                   })
+            // if none of the above states are matched, use this as the fallback
+            $urlRouterProvider.otherwise('/menus/home');
+        });
